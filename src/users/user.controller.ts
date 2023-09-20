e61fdab6413752428060e113c5d2f228a133b85e
kimhoang115 <hoangtran@dosiinvn.com>
2023-09-20 17:17:09 +0700

    u

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,18 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UserDto } from '../user.dto';
 
 @Controller('users')
 export class UserController {
-  @Get()
-  getAllUser() {
-    return [
-      {
-        name: 'test 1',
-        age: 1,
-      },
-      {
-        name: 'test 2',
-        age: 3,
-      },
-    ];
+  // @Get()
+  // getAllUser() {
+  //   return [
+  //     {
+  //       name: 'test 1',
+  //       age: 1,
+  //     },
+  //     {
+  //       name: 'test 2',
+  //       age: 3,
+  //     },
+  //   ];
+  // }
+  @Post()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  createUser(@Body() user: UserDto): UserDto {
+    return {
+      username: 'test',
+      password: 'test',
+    };
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: number) {
+    console.log(id);
+    return 'test';
   }
 }
